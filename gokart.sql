1a55182d3d860d2bcb025b5064b12d048f6accd0
asher-ma <asherhmangel@gmail.com>
2025-03-16 17:29:25 -0700

    Sample transactions added

--- gokart.sql
+++ gokart.sql
@@ -1,29 +1,36 @@
 -- Asher Mangel
 -- Rental Go Kart League
 
+CREATE DATABASE IF NOT EXISTS Go_Kart_League;
+USE Go_Kart_League;
+
+DROP TABLE IF EXISTS Race_Result;
+DROP TABLE IF EXISTS Booking;
+DROP TABLE IF EXISTS Maintenance;
+DROP TABLE IF EXISTS Driver;
+DROP TABLE IF EXISTS Race;
+DROP TABLE IF EXISTS Go_Kart;
+DROP TABLE IF EXISTS Mechanic;
+
 -- Driver entity
-DROP TABLE Driver;
 CREATE TABLE Driver
     (   driver_id       INT PRIMARY KEY,
         fname           VARCHAR(20) NOT NULL,
         lname           VARCHAR(20) NOT NULL,
-        rating          INT)
+        rating          INT);
 
 -- Race entity
-DROP TABLE Race;
 CREATE TABLE Race
     (   race_id         INT PRIMARY KEY,
         date            TEXT NOT NULL,
-        track           VARCHAR(20) NOT NULL)
+        track           VARCHAR(20) NOT NULL);
 
 -- Go Kart entity
-DROP TABLE Go_Kart;
 CREATE TABLE Go_Kart
     (   kart_num        INT PRIMARY KEY,
-        class           INT NOT NULL)
+        class           INT NOT NULL);
 
 -- Booking entity
-DROP TABLE Booking;
 CREATE TABLE Booking
     (   booking_id      INT PRIMARY KEY,
         date            TEXT NOT NULL,
@@ -32,17 +39,15 @@ CREATE TABLE Booking
         kart_num        INT NOT NULL,
         FOREIGN KEY (driver_id) REFERENCES Driver(driver_id),
         FOREIGN KEY (race_id) REFERENCES Race(race_id),
-        FOREIGN KEY (kart_num) REFERENCES Go_Kart(kart_num))
+        FOREIGN KEY (kart_num) REFERENCES Go_Kart(kart_num));
 
 -- Mechanic entity
-DROP TABLE Mechanic;
 CREATE TABLE Mechanic
     (   mech_id         INT PRIMARY KEY,
         fname           VARCHAR(20) NOT NULL,
-        lname           VARCHAR(20) NOT NULL)
+        lname           VARCHAR(20) NOT NULL);
 
 -- Maintenance entity
-DROP TABLE Maintenance;
 CREATE TABLE Maintenance
     (   work_id         INT PRIMARY KEY,
         kart_num        INT,
@@ -50,10 +55,9 @@ CREATE TABLE Maintenance
         hours           INT NOT NULL,
         date            TEXT NOT NULL,
         FOREIGN KEY (kart_num) REFERENCES Go_Kart(kart_num),
-        FOREIGN KEY (mech_id) REFERENCES Mechanic(mech_id))
+        FOREIGN KEY (mech_id) REFERENCES Mechanic(mech_id));
 
 -- Race Result weak entity
-DROP TABLE Race_Result;
 CREATE TABLE Race_Result
     (   driver_id       INT,
         race_id         INT,
@@ -61,7 +65,7 @@ CREATE TABLE Race_Result
         finish_pos      INT NOT NULL,
         PRIMARY KEY (driver_id, race_id),
         FOREIGN KEY (driver_id) REFERENCES Driver(driver_id),
-        FOREIGN KEY (race_id) REFERENCES Race(race_id))
+        FOREIGN KEY (race_id) REFERENCES Race(race_id));
 
 
 
@@ -75,13 +79,14 @@ VALUES
     (44, 'Lewis', 'Hamilton', 2600),
     (82, 'Asher', 'Mangel', 300),
     (10, 'David', 'Coulthard', 1400),
-    (1, 'Max', 'Verstappen', 3000);
+    (1, 'Max', 'Verstappen', 3000),
+    (4, 'Lando', 'Norris', 2800);
 
 -- Sample races
 INSERT INTO Race (race_id, date, track)
 VALUES
-    (1, '2025-03-16', 'Melbourne Grand Prix Circuit'),
-    (3, '2024-03-23', 'Melbourne Grand Prix Circuit'),
+    (1, '2025-03-16', 'Albert Park Circuit'),
+    (3, '2024-03-23', 'Albert Park Circuit'),
     (12, '2024-07-07', 'Silverstone Circuit'),
     (50, '2024-12-20', 'K1 Speed Bellevue'),
     (51, '2024-12-20', 'K1 Speed Bellevue'),
@@ -94,7 +99,8 @@ VALUES
     (44, 1),
     (82, 5),
     (10, 2),
-    (1, 1);
+    (1, 1),
+    (4, 1);
 
 -- Sample bookings
 INSERT INTO Booking (booking_id, date, driver_id, race_id, kart_num)
@@ -106,6 +112,7 @@ VALUES
     (4, '2025-03-16', 1, 2, 1),
     (5, '2024-12-20', 82, 50, 82),
     (6, '2024-12-20', 82, 51, 82),
+    (7, '2025-03-13', 4, 1, 4);
 
 -- Sample mechanics
 INSERT INTO Mechanic (mech_id, fname, lname)
@@ -131,63 +138,104 @@ VALUES
     (55, 3, 2, 1),
     (44, 12, 2, 1),
     (1, 1, 3, 2),
+    (4, 1, 1, 1),
     (44, 1, 8, 10),
-    (82, 50, 4, 2)
+    (82, 50, 4, 1),
     (82, 51, 8, 12);
 
 
 
 -- Sample data queries
+/*
+I tried to use \G to better space out the columns but it just made every entry
+take up a lot of space and this way is printing them including headers anyway
+*/
+
+/*
 SELECT *
 FROM Driver
-LIMIT 5\G;
+LIMIT 5;
 
 SELECT *
 FROM Race
-LIMIT 5\G;
+LIMIT 5;
 
 SELECT *
 FROM Go_Kart
-LIMIT 5\G;
+LIMIT 5;
 
 SELECT *
 FROM Booking
-LIMIT 5\G;
+LIMIT 5;
 
 SELECT *
 FROM Mechanic
-LIMIT 5\G;
+LIMIT 5;
 
 SELECT *
 FROM Maintenance
-LIMIT 5\G;
+LIMIT 5;
 
 SELECT *
 FROM Race_Result
-LIMIT 5\G;
+LIMIT 5;
+*/
 
 
-/*
 -- Sample transactions
 
--- Select all races with a competing driver that has a rating above 2000 
-SELECT DISTINCT R.track
+-- Select all races with competing drivers with average ratings above 2000 
+SELECT 'High rated races:' AS '';
+SELECT DISTINCT R.track, AVG(D.rating) AS avg_rating
 FROM Race AS R
 JOIN Booking AS B ON R.race_id = B.race_id
 JOIN Driver AS D ON B.driver_id = D.driver_id
-WHERE D.rating > 2000
-LIMIT 5\G;
+GROUP BY R.track
+HAVING AVG(D.rating) > 2000
+LIMIT 5;
 
--- Select all drivers names that have raced with karts in class 1
-SELECT D.fname, D.lname
+-- Select all drivers names that have raced the most with class 1 karts
+SELECT 'Most experienced class 1 Drivers:' AS '';
+
+SELECT DISTINCT D.fname, D.lname, COUNT(*) AS num_races
 FROM Driver AS D
-JOIN Booking AS B ON D.
+JOIN Booking AS B ON D.driver_id = B.driver_id
+JOIN Go_Kart AS K ON B.kart_num = K.kart_num
+WHERE K.class = 1
+GROUP BY D.fname, D.lname
+ORDER BY COUNT(*) DESC
+LIMIT 5;
 
--- Select the name of drivers that have finished first
+-- Select each track and group with each driver that has won at that track
+SELECT 'Race Winners:' AS '';
 
+SELECT DISTINCT R.track, D.fname, D.lname
+FROM Driver AS D
+JOIN Race_Result AS RR ON D.driver_id = RR.driver_id
+JOIN Race AS R ON RR.race_id = R.race_id
+WHERE RR.finish_pos = 1
+GROUP BY R.track, D.fname
+LIMIT 5;
 
--- Select the names of drivers that have finished in a lower position than where they started 
+-- Select the names of drivers that have lost the most 
+SELECT 'Drivers who have lost the most places:' AS '';
 
+SELECT DISTINCT D.fname, D.lname, (RR.start_pos - RR.finish_pos) AS pos_lost
+FROM Driver AS D
+JOIN Race_Result AS RR ON D.driver_id = RR.driver_id
+WHERE RR.finish_pos < RR.start_pos
+GROUP BY D.fname, D.lname
+ORDER BY pos_lost DESC
+LIMIT 5;
 
--- Select the names of mechanics who have worked a kart drived by a driver with a rating above 2000
-*/
+-- Select the names of mechanics in order by the average rating of their drivers
+SELECT 'High level mechanics' AS '';
+
+SELECT DISTINCT M.fname, M.lname
+FROM Mechanic AS M
+JOIN Maintenance AS MW ON M.mech_id = MW.mech_id
+JOIN Booking AS B ON MW.kart_num = B.kart_num
+JOIN Driver AS D ON B.driver_id = D.driver_id
+GROUP BY M.fname, M.lname
+ORDER BY AVG(D.rating) DESC
+LIMIT 5;
